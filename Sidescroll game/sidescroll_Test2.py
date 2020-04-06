9f17aea0cce9f0c278c19a61f185a8e3374d4cd3
B-B-2-1 <josephbaby.baby@gmail.com>
2020-04-06 23:23:56 +0530

    small change in level

--- Sidescroll game/sidescroll_Test2.py
+++ Sidescroll game/sidescroll_Test2.py
@@ -109,7 +109,7 @@ rectangles.append(pygame.Rect(450,50,50,200))
 rectangles.append(pygame.Rect(550,150,300,300))
 rectangles.append(pygame.Rect(550,50,50,100))
 rectangles.append(pygame.Rect(900,400,500,50))
-rectangles.append(pygame.Rect(1050,50,50,300)) 
+rectangles.append(pygame.Rect(1045,50,50,300)) 
 rectangles.append(pygame.Rect(1150,50,50,300)) 
 rectangles.append(pygame.Rect(1200,50,500,50)) 
 ##################################################################################
